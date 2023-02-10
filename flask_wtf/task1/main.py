02f7b68d647a0ee62f7e8ac0499690f849eb166c
Dany <kiper.daniel2006@gmail.com>
2023-02-10 19:19:46 +0300

    commit

---
+++ flask_wtf/task1/main.py
@@ -0,0 +1,14 @@
+import flask
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template("index.html", title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
