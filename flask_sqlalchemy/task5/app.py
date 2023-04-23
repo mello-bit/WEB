47ed604bb354d1db5396228ce27511b0c82a626d
Dany <86149558+QBoff@users.noreply.github.com>
2023-04-23 16:31:39 +0300

    class work

---
+++ flask_sqlalchemy/task5/app.py
@@ -0,0 +1,92 @@
+from db_session import global_init, create_session
+from users import User
+from jobs import Job
+from flask import Flask, render_template, redirect
+from forms import LoginForm, SignUpForm, AddJobs
+from flask_login import LoginManager, login_user
+
+
+global_init("task5.db")  # bd лежит в папке WEB т.к venv тоже лежит в WEB
+db_sess = create_session()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def showJobsList():
+    jobs = db_sess.query(Job).all()
+    
+    return render_template('jobs.html', jobs=jobs)
+
+
+@app.route('/add_job', methods=["GET", "POST"])
+def addJob():
+    addJob = AddJobs()
+    
+    if addJob.validate_on_submit():
+        job = Job(
+            jobTitle=addJob.jobTitle.data,
+            teamLeaderId=addJob.teamLeaderId.data,
+            workSize=addJob.workSize.data,
+            collaborators=addJob.collaborators.data,
+            isFinished=addJob.isFinished.data
+        )
+        
+        db_sess.add(job)
+        db_sess.commit()
+        return redirect('/')
+
+    return render_template('jobsForm.html', title="Добавление работы", form=addJob)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    global jobs
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        user = db_sess.query(User).filter(
+            User.email == loginForm.email.data).first()
+
+        if user:
+            login_user(user, remember=loginForm.remember_me.data)
+            print(user.nickname)
+            jobs = user.jobsList
+            print(jobs)
+            return redirect('/')
+
+        return render_template('login.html', message="Неправильный логин или пароль", form=loginForm)
+
+    return render_template('login.html', title="Авторизация", form=loginForm)
+
+
+@app.route('/sign_up', methods=["GET", "POST"])
+def signUp():
+    signUpForm = SignUpForm()
+    print("SignUp")
+
+    if signUpForm.is_submitted():
+        user = User(email=signUpForm.email.data,
+                    password=signUpForm.password.data,
+                    jobsList=signUpForm.jobsList.data,
+                    nickname=signUpForm.nickname.data)
+
+        print(user, 1)
+        db_sess.add(user)
+        db_sess.commit()
+        print(user, 2)
+
+        return render_template('signUp.html', message="Everything is ok", form=signUpForm)
+
+    return render_template('signUp.html', title="Авторизация", form=signUpForm)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ flask_sqlalchemy/task7/app.py
@@ -0,0 +1,123 @@
+from db_session import global_init, create_session
+from users import User
+from jobs import Job
+from flask import Flask, render_template, redirect
+from forms import LoginForm, SignUpForm, AddJobs
+from flask_login import LoginManager, login_user
+import hashlib
+
+
+global_init("ultimate.db")  # bd лежит в папке WEB т.к venv тоже лежит в WEB
+db_sess = create_session()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['salt'] = '5gz'
+nickname = None
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/')
+def showJobsList():
+    jobs = db_sess.query(Job).all()
+
+    if nickname is not None:
+        return render_template('jobs.html', jobs=jobs, nickname=nickname)
+
+    return render_template('jobs.html', jobs=jobs)
+
+
+@app.route('/add_job', methods=["GET", "POST"])
+def addJob():
+    addJob = AddJobs()
+
+    if addJob.validate_on_submit():
+        job = Job(
+            jobTitle=addJob.jobTitle.data,
+            teamLeaderId=addJob.teamLeaderId.data,
+            workSize=addJob.workSize.data,
+            collaborators=addJob.collaborators.data,
+            isFinished=addJob.isFinished.data
+        )
+
+        db_sess.add(job)
+        db_sess.commit()
+        return redirect('/')
+
+    if nickname is not None:
+        return render_template('jobsForm.html', title="Добавление работы", form=addJob, nickname=nickname)
+
+    return render_template('jobsForm.html', title="Добавление работы", form=addJob)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    global nickname
+
+    loginForm = LoginForm()
+
+    if loginForm.validate_on_submit():
+        user = db_sess.query(User).filter(
+            User.email == loginForm.email.data).first()
+        if user:
+            databasePassword = user.password
+            
+            if databasePassword == \
+                    hashlib.md5((loginForm.password.data + app.config['salt']).encode()).hexdigest():
+
+                login_user(user, remember=loginForm.remember_me.data)
+                nickname = user.nickname
+                return redirect('/')
+
+        return render_template('login.html', message="Неправильный логин или пароль", form=loginForm, nickname=nickname)
+
+    if nickname is not None:
+        return render_template('login.html', title="Авторизация", form=loginForm, nickname=nickname)
+
+    return render_template('login.html', title="Авторизация", form=loginForm)
+
+
+@app.route('/sign_up', methods=["GET", "POST"])
+def signUp():
+    global nickname
+
+    signUpForm = SignUpForm()
+    print("SignUp")
+
+    if signUpForm.is_submitted():
+        databasePassword = hashlib.md5(
+            (signUpForm.password.data + app.config['salt']).encode())
+        user = User(email=signUpForm.email.data,
+                    password=databasePassword.hexdigest(),
+                    jobsList=signUpForm.jobsList.data,
+                    nickname=signUpForm.nickname.data)
+
+        print(user, 1)
+        db_sess.add(user)
+        db_sess.commit()
+        print(user, 2)
+        nickname = user.nickname
+        return render_template('signUp.html', message="Everything is ok", form=signUpForm, nickname=nickname)
+
+    if nickname is not None:
+        return render_template('signUp.html', title="Авторизация", form=signUpForm, nickname=nickname)
+
+    return render_template('signUp.html', title="Авторизация", form=signUpForm)
+
+
+@app.route('/logout')
+def logout():
+    global nickname
+
+    nickname = None
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
