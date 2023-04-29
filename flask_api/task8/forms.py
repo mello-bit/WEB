878262d1d4743b2c7707f00187d41d655e5c44b1
Dany <86149558+QBoff@users.noreply.github.com>
2023-04-29 11:13:29 +0300

    task7

---
+++ flask_api/task8/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, StringField
+from wtforms import BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+    
+
+class SignUpForm(FlaskForm):
+    nickname = StringField('Имя')
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    jobsList = StringField('Введите список ваших работ :)', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Зарегистрироваться')
+    
+    
+class AddJobs(FlaskForm):
+    jobTitle = EmailField('Job Title', validators=[DataRequired()])
+    teamLeaderId = StringField('Team Leader ID', validators=[DataRequired()])
+    workSize = StringField('Work Size', validators=[DataRequired()])
+    collaborators = StringField('Collaborators', validators=[DataRequired()])
+    isFinished = BooleanField('Is Finished?')
+    submit = SubmitField('Submit')
